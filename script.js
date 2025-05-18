efc7ceaeffe12729c45d9335bf9ad6412ea869ba
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-05-18 15:04:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-async function hello(){
-	try {
-		let response = await fetch('employees');
-		let data = await response.json();
-		return data;
-	} catch (error) {
-		console.log(error);
-	}
-}
-
-const jsonData = hello();
-jsonData = jsonData.sort((a,b)=>a.age-b.age);
-console.log(jsonData);
+// Asynchronously fetch and process employee data
+fetch('employees.json')
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    return response.json();
+  })
+  .then(employees => {
+    // Sort employees by age in ascending order
+    employees.sort((a, b) => a.age - b.age);
+    
+    // Log sorted employee list
+    console.log("Sorted Employees by Age:");
+    console.log(employees);
+  })
+  .catch(error => {
+    console.error('There was a problem fetching the employee data:', error);
+  });
