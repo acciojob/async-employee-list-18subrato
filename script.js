0c9823d45402860c48c0d2fe01bb8f94c3f86d15
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2025-05-18 14:58:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ async function hello(){
 	try {
 		let response = await fetch('employees');
 		let data = await response.json();
-		data.sort((a,b)=>a.age-b.age);
 		return data;
 	} catch (error) {
 		console.log(error);
 	}
 }
 
-console.log(hello());
+const jsonData = hello();
+jsonData = jsonData.sort((a,b)=>a.age-b.age);
+console.log(jsonData);
